842341c7263d2bb3e994e9e95baccb205eb5d201
tsb1995 <taylorberukoff@gmail.com>
2022-03-10 23:49:27 -0800

    add basic BlazeFace functionality

--- js/tfjs.js
+++ js/tfjs.js
@@ -1,7 +1,14 @@
+// Coco key DOM elements
 const cocoVideo = document.getElementById('cocoWebcam');
-const liveView = document.getElementById('liveView');
+const cocoLiveView = document.getElementById('cocoLiveView');
 const cocoDemoSection = document.getElementById('cocoDemo');
 const cocoEnableWebcamButton = document.getElementById('cocoWebcamButton');
+
+// BlazeFace key DOM elements
+const blazeVideo = document.getElementById('blazeWebcam');
+const blazeLiveView = document.getElementById('blazeLiveView');
+const blazeDemoSection = document.getElementById('blazeDemo');
+const blazeEnableWebcamButton = document.getElementById('blazeWebcamButton');
 
 // Check if webcam access is supported.
 function getUserMediaSupported() {
@@ -10,32 +17,41 @@ function getUserMediaSupported() {
   }
   
   // If webcam supported, add event listener to button for when user
-  // wants to activate it to call enableCam function which we will 
+  // wants to activate it to call enableCocoCam function which we will 
   // define in the next step.
   if (getUserMediaSupported()) {
-    cocoEnableWebcamButton.addEventListener('click', enableCam);
+    cocoEnableWebcamButton.addEventListener('click', enableCocoCam);
+    blazeEnableWebcamButton.addEventListener('click', enableBlazeCam)
   } else {
     console.warn('getUserMedia() is not supported by your browser');
 }
 
 
 
-// Before we can use COCO-SSD class we must wait for it to finish
+// Before we can use COCO-SSD or BlazeFace class we must wait for it to finish
 // loading. Machine Learning models can be large and take a moment 
 // to get everything needed to run.
 // Note: cocoSsd is an external object loaded from our index.html
 // script tag import
 cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
+  cocoModel = loadedModel;
   // Show demo section now model is ready to use.
   cocoDemoSection.classList.remove('invisible');
 });
 
-// Occurs once eventListener detecks click on enableCam
+blazeface.load().then(function (loadedModel) {
+  blazeModel = loadedModel;
+  // Show demo section now model is ready to use.
+  blazeDemoSection.classList.remove('invisible');
+});
+
+
+
+// Occurs once eventListener detects click on enableCocoCam
 // Enable the live webcam view and start classification.
-function enableCam(event) {
-    // Only continue if the COCO-SSD has finished loading.
-    if (!model) {
+function enableCocoCam(event) {
+    // Only continue if the COCO-SSD and BlazeFace have finished loading.
+    if (!(cocoModel)) {
       return;
     }
     
@@ -54,16 +70,16 @@ function enableCam(event) {
     });
 }
 
-var children = [];
+var cocoChildren = [];
 
 function cocoPredictWebcam() {
   // Now let's start classifying a frame in the stream.
-  model.detect(cocoVideo).then(function (predictions) {
+  cocoModel.detect(cocoVideo).then(function (predictions) {
       // Remove any highlighting we did previous frame.
-      for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
+      for (let i = 0; i < cocoChildren.length; i++) {
+        cocoLiveView.removeChild(cocoChildren[i]);
       }
-      children.splice(0);
+      cocoChildren.splice(0);
       
       // Now lets loop through predictions and draw them to the live view if
       // they have a high confidence score.
@@ -85,10 +101,10 @@ function cocoPredictWebcam() {
               + predictions[n].bbox[2] + 'px; height: '
               + predictions[n].bbox[3] + 'px;';
 
-          liveView.appendChild(highlighter);
-          liveView.appendChild(p);
-          children.push(highlighter);
-          children.push(p);
+              cocoLiveView.appendChild(highlighter);
+              cocoLiveView.appendChild(p);
+          cocoChildren.push(highlighter);
+          cocoChildren.push(p);
       }
     }
     // Call this function again to keep predicting when the browser is ready.
@@ -96,6 +112,87 @@ function cocoPredictWebcam() {
   });
 }
 
-// Store the resulting model in the global scope of our app.
-var model = undefined;
 
+
+
+
+
+
+// Enable the live webcam view and start classification.
+function enableBlazeCam(event) {
+  // Only continue if the COCO-SSD has finished loading.
+  if (!blazeModel) {
+    return;
+  }
+  
+  // Hide the button once clicked.
+  event.target.classList.add('removed');  
+  
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
+    video: true
+  };
+
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    blazeVideo.srcObject = stream;
+    blazeVideo.addEventListener('loadeddata', blazePredictWebcam);
+  });
+}
+
+var blazeChildren = [];
+
+function blazePredictWebcam() {
+  // Now let's start classifying a frame in the stream.
+  const returnTensors = false;
+  blazeModel.estimateFaces(blazeVideo, returnTensors).then(function (predictions) {
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < blazeChildren.length; i++) {
+      blazeLiveView.removeChild(blazeChildren[i]);
+    }
+    blazeChildren.splice(0);
+
+    console.log("test");
+    if (!!predictions[0]) {
+      console.log(predictions[0]);
+      console.log(predictions[0].probability);
+    }
+
+    
+    // Now lets loop through predictions and draw them to the live view if
+    // they have a high confidence score.
+    for (let n = 0; n < predictions.length; n++) {
+      // If we are over 66% sure we are sure we classified it right, draw it!
+      if (predictions[n].probability > 0.66) {
+        let start = predictions[n].topLeft;
+        let end = predictions[n].bottomRight;
+        let size = [end[0] - start[0], end[1] - start[1]];
+
+        console.log(size);
+
+        const highlighter = document.createElement('div');
+        highlighter.setAttribute('class', 'highlighter');
+        highlighter.style = 'left: ' + start[0] + 'px; top: '
+            + start[1] + 'px; width: ' 
+            + size[0] + 'px; height: '
+            + size[1] + 'px;';
+
+        blazeLiveView.appendChild(highlighter);
+        blazeChildren.push(highlighter);
+      }
+    }
+    
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(blazePredictWebcam);
+  });
+}
+
+
+
+
+
+
+
+// Store the resulting model in the global scope of our app.
+var cocoModel = undefined;
+var blazeModel = undefined;
